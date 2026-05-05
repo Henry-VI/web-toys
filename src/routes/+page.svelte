bc0eb73878f117836f0fe5c245ca20f48933b20e
Henry-VI <henryvaniersel@proton.me>
2026-05-04 23:48:26 -0700

    the "add EU stars" part took waayy to long

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,18 @@
-<div id="center" class="bg-white h-screen mx-auto w-full md:w-0">
-    <div id="hiddentitle" class="md:opacity-0 mx-auto flex justify-center">
-      <img src="mapleleaf.svg" alt="Canadian Maple Leaf" class="animate-[spin_2s_ease-in-out_2_1s,fadeout_1s_5s_forwards] md:animate-[slideup_1s_4s_forwards,fadeout_1s_5s_forwards] w-1/2 md:w-auto mt-[calc(50vh-25%)] md:mt-0 md:h-[50vh] md:translate-y-[50vh] md:opacity-0">
+<div id="center" class="bg-white h-screen mx-auto w-full md:w-0 overflow-hidden">
+    <div id="hiddentitle" class="md:opacity-0 mx-auto flex justify-center flex-col">
+      <img src="mapleleaf.svg" alt="Canadian Maple Leaf" class="animate-[spin_2s_ease-in-out_2_1s,fadeout_1s_5s_forwards] md:animate-[slideup_1s_4s_forwards,fadeout_1s_7s_forwards] w-1/2 mx-auto md:mx-0 md:w-auto mt-[calc(50vh-25%)] md:mt-0 md:h-[50vh] md:translate-y-[50vh] md:opacity-0">
+      <span class="translate-y-[-85%] md:translate-y-[-60vh]"> <!-- I don't even know okay -->
+      <img src="euro_stars.svg" alt="12 yellow stars" class="animate-[spin_2s_ease-in-out_2_1s,fadeout_1s_5s_forwards] md:opacity-0 w-[80vw] md:w-[45vw] m-auto md:translate-y-[50vh] md:animate-[slideupeu_1s_4s_forwards,fadeout_1s_7s_forwards,spineu_3s_5s_forwards]">
+      </span>
     </div>
-    <div id="content" class="opacity-0 animate-[fadein_1s_6s_forwards] absolute top-0">
+    <div id="content" class="opacity-0 animate-[fadein_1s_8s_forwards] absolute top-0 w-full h-full">
       <!-- This is where the actual content is! -->
-      <div>
+      <div class="text-center">
         Hello, World!
         <br>
-        Welcome to the Canadian world order
+        Website theory: if anything has a website, it automatically becomes relevant
+        <br>
+        #CAEU
       </div>
   </div>
 </div>
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,1 @@
+<img src="euro_stars.svg" alt="12 yellow stars" class="opacity-0 md:opacity-100 w-[45vw] mx-auto animate-spin" style="animation: spin linear 20s infinite;">
