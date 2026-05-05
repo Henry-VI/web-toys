9dea3a7ea2d535101e9865a58088479b0f3f3cf1
Henry-VI <henryvaniersel@proton.me>
2026-05-04 19:31:28 -0700

    nonsense

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,5 @@
-<div id="center" class="bg-white h-screen mx-auto w-0">
-    <div id="hiddentitle" class="opacity-0 text-center">Hello, World!</div>
+<div id="center" class="bg-white opacity-100 h-screen mx-auto w-full md:w-0">
+    <div id="hiddentitle" class="md:opacity-0 mx-auto flex justify-center">
+      <img src="mapleleaf.svg" alt="Canadian Maple Leaf" class="animate-[spin_2s_ease-in-out_2_1s,fadeout_1s_5s_forwards] md:animate-none w-1/2 md:w-auto mt-[calc(50vh-25%)]">
+    </div>
 </div>
