0e1298463c98293857a1344d735e39c45ddf56f6
Henry-VI <henryvaniersel@proton.me>
2026-05-04 20:08:37 -0700

    wip loading thing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,13 @@
-<div id="center" class="bg-white opacity-100 h-screen mx-auto w-full md:w-0">
+<div id="center" class="bg-white h-screen mx-auto w-full md:w-0">
     <div id="hiddentitle" class="md:opacity-0 mx-auto flex justify-center">
-      <img src="mapleleaf.svg" alt="Canadian Maple Leaf" class="animate-[spin_2s_ease-in-out_2_1s,fadeout_1s_5s_forwards] md:animate-none w-1/2 md:w-auto mt-[calc(50vh-25%)]">
+      <img src="mapleleaf.svg" alt="Canadian Maple Leaf" class="animate-[spin_2s_ease-in-out_2_1s,fadeout_1s_5s_forwards] md:animate-[slideup_1s_4s_forwards,fadeout_1s_5s_forwards] w-1/2 md:w-auto mt-[calc(50vh-25%)] md:mt-0 md:translate-y-[50vh] md:opacity-0">
     </div>
+    <div id="content" class="opacity-0 animate-[fadein_1s_6s_forwards] absolute top-0">
+      <!-- This is where the actual content is! -->
+      <div>
+        Hello, World!
+        <br>
+        Welcome to the Canadian world order
+      </div>
+  </div>
 </div>
